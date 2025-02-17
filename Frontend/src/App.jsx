5258546533a3e3ff191d0ac219b0d94826bf19d0
Abhi-Aku <abhi2003av@gmail.com>
2025-02-17 15:11:09 +0530

    Data
    Update Data

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-          Hello world!
-       </h1>
+    <div className='max-w-screen-xl mx-auto px-10 '>
+      <Navbar/>
+      <main className=' mt-8 min-h-screen '>
+        <Outlet/>
+      </main>
+      
+      <footer className="border-t mt-32 pt-12 pb-32 px-4 lg:px-0 text-black font-bold ">
+    <div className="flex">
+
+      <div className="w-full md:w-1/3 lg:w-1/4">
+        <h6 className="font-semibold text-gray-700 mb-4">Company</h6>
+        <ul>
+          <li> <a href="#" className="block text-gray-600 py-2">Team</a> </li>
+          <li> <a href="#" className="block text-gray-600 py-2">About us</a> </li>
+          <li> <a href="#" className="block text-gray-600 py-2">Press</a> </li>
+        </ul>
+      </div>
+
+      <div className="w-full md:w-1/3 lg:w-1/4">
+        <h6 className="font-semibold text-gray-700 mb-4">Content</h6>
+        <ul>
+          <li> <a href="#" className="block text-gray-600 py-2">Blog</a> </li>
+          <li> <a href="#" className="block text-gray-600 py-2">Privacy Policy</a> </li>
+          <li> <a href="#" className="block text-gray-600 py-2">Terms & Conditions</a> </li>
+          <li> <a href="#" className="block text-gray-600 py-2">Documentation</a> </li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
+
     </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from  "react-router-dom";
+import Home from './Page/Home/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [{
+      path: '/',
+      element: <Home/>,
+    }]
+      
+    },
+])
+
+
+
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
